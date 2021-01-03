4d6b82a568a983cb90ff2d88789cb1a2d9923676
furkanarslan <furkan.arslan@ozu.edu.tr>
2021-01-03 11:07:40 +0000

    run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-python train.py --dataset shoes  --config config/shoes.json --mode train --apex --seed 0
-python train.py --dataset shoes  --config config/shoes.json --mode trainval --apex --seed 0
+
+python train.py --dataset shoes  --config config/shoes.json --mode train --apex --seed 0 --version 0.6.3
+python train.py --dataset shoes  --config config/shoes.json --mode trainval --apex --seed 0 --version 0.6.3
